925d3f491777a5362725a7bf6d9ade346647d332
haibozhou <chuckzhb@hotmail.com>
2021-09-13 23:51:57 +0800

    create project

---
+++ WeChat Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeChat Clone
+//
+//  Created by HaiboZhou on 2021/9/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
